a829d6e383f7744afd7aa8c9007dcac44aea568f
mechoutes <35970200+mechoutes@users.noreply.github.com>
2019-02-24 11:19:22 +0300

    Add files via upload

---
+++ a.bat
@@ -0,0 +1,125 @@
+@echo off
+title Diyalog
+echo -Section ONE- 
+echo Before the start;
+echo When I stop the talking, you will talking. With your religion.
+echo what is your religion?
+set/p religion=
+echo I dont really care about differences in religions. Your religion is real. And him religion is real to.
+echo Are you ready to section Two?
+set/p ready=
+echo oh you %ready%
+
+
+echo -Section TWO- 
+echo Dont forget, it is reality.
+echo Do u believe it?
+set/p believe?=
+echo it is doesnt matter, to hear it all. Because There is no matter what u believe it. Like I said, it is the reality.
+echo The rules is simple.
+echo Every section get his selections.
+echo there is only one key to get control.
+echo Did u understand?
+Set/p understand=
+echo I Like jokes Mr "%understand%". 
+echo type a keyword to get other section.
+set/p ok=
+
+echo -Section THREE-
+echo dont forget,%ok%
+echo there is no rules.
+echo what do u think about doesn't exist the limits?
+set/p ?=
+echo this is %?% universe.
+echo Should am ý sure of it?
+set/p should=
+echo mrs %ok% I didnt look your answer. And why? Just think about it.
+echo is there any idea on your head about why?
+set/p why=
+
+echo -Section FOUR-
+echo Hello Mr and Mrs. %ok%
+echo this section is getting hardy more too.
+
+echo -Section FÝVE-
+echo that section is my lucky number. And what is your lucky number?
+set/p lucky=
+
+echo -Section %lucky%-
+echo You have too far to get key.
+echo Where is the key, can u see it?
+set/p yesorno=
+echo "%yesorno%" sorry to hear about it.
+echo Why do you make me sad?
+echo Just make it happy you F099IVCKVOC
+
+echo -Section 6-
+echo Welcome to section 6
+echo And that section ends here.
+
+echo -Section SÝX-
+echo Welcome to new section.
+echo Oh I see, You got too many skills.
+echo Can you show me that skills
+set/p skill=
+echo oh your skill is %skill%
+echo Thank you for sharing me that skill %ok%.
+
+echo -Reality time-
+echo Hello there is time machine.
+echo did you believe time?
+set/p noah=
+echo Oh I understand, I can see your time now %time%.
+
+echo -Section 1-
+echo How old are you?
+set/p old=
+echo oh look at that, %ok% is %old% years old. Ýts weird.
+
+echo -Section 2-
+echo What is your full name?
+set/p fullname=
+echo oh %fullname% nice to meet you.
+
+echo -Section 3-
+echo What is your gender?
+set/p must=
+echo it must be.
+echo I have too many idea on my mind but, gender is a gift. A gift like how do you like to used.
+
+echo -Section 4-
+echo Do you believe me?
+set/p me?=
+echo do I ask it before?
+set/p ask=
+echo oh, I love your reality.
+
+echo -Section KEY-
+echo Do u ready to face to face with case?
+set/p ftf=
+echo okey %must% %fullname%.
+echo You have to many name on reality.
+echo Ýts changing by u.
+echo say start.
+set/p startt=
+echo created %ok%.
+echo luck change %lucky%
+echo full name is %fullname%
+echo old is %old%
+echo time is %time%
+echo And time is now.
+
+
+echo A
+set/p A=
+echo L
+set/p L=
+echo Merhaba %A% %L%
+echo Say your key
+set/p key=
+echo There is your key; 
+color a
+mode1000
+:start
+echo %key% %key% %key% %key% %key% %key% %key% %key% %key% %key% %key% %key% %key% %key% %key% %key% %key% %key% D O R A H
+goto start
